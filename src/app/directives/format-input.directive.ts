ee9e201e30d6174afe4d53a51afe2ec476d27ab6
M1ch4l11 <lojko.michal11@gmail.com>
2024-05-06 11:27:22 +0200

    Fix: Delet input content

--- src/app/directives/format-input.directive.ts
+++ src/app/directives/format-input.directive.ts
@@ -8,12 +8,23 @@ export class FormatInputDirective {
   @Input() type!: string;
   constructor(private el: ElementRef) {}
 
-  @HostListener('input', ['$event']) onInput(event: any) {
-    const valueWithSpaces: String = this.insertSpaces(
-      event.target.value,
+  @HostListener('input', ['$event.target'])
+  onInput(target: HTMLInputElement) {
+    const previousValue = target.getAttribute('data-previous-value') || '';
+    const currentValue = target.value;
+
+    if (currentValue.length < previousValue.length) {
+      target.setAttribute('data-previous-value', currentValue);
+      return;
+    }
+
+    const valueWithSpaces: string = this.insertSpaces(
+      target.value,
       this.type === 'countries' ? 2 : 4
     );
+
     this.el.nativeElement.value = valueWithSpaces.toUpperCase();
+    target.setAttribute('data-previous-value', valueWithSpaces);
   }
 
   insertSpaces(inputValue: string, countChar: number): string {
