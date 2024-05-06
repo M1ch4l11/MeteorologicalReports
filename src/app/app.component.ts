8820dccdbe0dd8c605d6d83fd423a57a8c0ba75e
M1ch4l11 <lojko.michal11@gmail.com>
2024-05-06 09:44:32 +0200

    Feat: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormComponent } from './components/form/form.component';
+import { TableComponent } from './components/table/table.component';
+import { ReportMessage } from './models/report';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormComponent, TableComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'MeteorologicalReports';
-}
+export class AppComponent {}
